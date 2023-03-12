dbf59b75530eac08d64703fe3607a2740bb08081
jdtroubat <jd.troubat@gmail.com>
2023-03-12 18:51:56 +0100

    "getComputerChoice" function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function getComputerChoice() {
+    const cpuChoices = ["Rock", "Paper", "Scissors"];
+    const randomCpu = Math.floor(Math.random() * cpuChoices.length);
+    return cpuChoices[randomCpu]; 
+}
 
+console.log(getComputerChoice());
