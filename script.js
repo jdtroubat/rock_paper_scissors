848172624e7bd4126c40be4cdb5d347ba315ee16
jdtroubat <jd.troubat@gmail.com>
2023-03-12 22:23:50 +0100

    Functional One Round Game + case insensitive choice

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+/** Random Cpu */
 function getComputerChoice() {
-    const cpuChoices = ["Rock", "Paper", "Scissors"];
-    const randomCpu = Math.floor(Math.random() * cpuChoices.length);
-    return cpuChoices[randomCpu]; 
+    let cpuChoice = ["rock", "paper", "scissors"];
+    let randomCpu = Math.floor(Math.random() * cpuChoice.length);
+    return cpuChoice[randomCpu]; 
 }
 
-console.log(getComputerChoice());
+/** Prompt playerChoice*/
+let playerChoice = prompt("Enter Rock, Paper, or Scissors");
+playerChoice = playerChoice.toLowerCase();
+
+/** playerChoice vs cpuChoice */
+function playerSelection(cpuChoice, playerChoice) {
+    if ((playerChoice === "rock" && cpuChoice === "scissors") ||
+        (playerChoice === "paper" && cpuChoice === "rock") ||
+        (playerChoice === "scissors" && cpuChoice === "paper")) {
+        return "You Win, " + playerChoice + " beats " + cpuChoice;
+    } else if (playerChoice === cpuChoice){
+        return "Tie Game, " + playerChoice + " is equal to " + cpuChoice;
+    } else {
+        return "You Loose, " + playerChoice + " is beaten by " + cpuChoice;
+    }
+    
+}
+/** Uppercase first Letter playerChoice and cpuChoice */
+playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+let cpuChoice = getComputerChoice();
+cpuChoice = cpuChoice.charAt(0).toUpperCase() + cpuChoice.slice(1);
+let result = playerSelection(cpuChoice, playerChoice);
+
+console.log(result);
